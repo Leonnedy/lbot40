c1eb9fabee08fc9515e942d1c67989fd2235f645
Leonnedy <81530232+Leonnedy@users.noreply.github.com>
2021-04-08 17:42:48 -0300

    Add files via upload

--- plugins/leaderboard.js
+++ plugins/leaderboard.js
@@ -1,34 +1,23 @@
-let handler = async (m, { conn, args, participants }) => {
-  let users = Object.entries(global.DATABASE.data.users).map(([key, value]) => {
-    return {...value, jid: key}
-  })
-  let sortedExp = users.map(toNumber('exp')).sort(sort('exp', true))
-  let sortedLim = users.map(toNumber('limit')).sort(sort('limit', true))
-  let sortedLevel = users.map(toNumber('level')).sort(sort('level', true))
-  let usersExp = sortedExp.map(enumGetKey)
-  let usersLim = sortedLim.map(enumGetKey)
-  let usersLevel = sortedLevel.map(enumGetKey)
-  console.log(participants)
-  let len = args[0] && args[0].length > 0 ? Math.min(100, Math.max(parseInt(args[0]), 5)) : Math.min(5, sortedExp.length)
+let handler = async (m, { conn, args }) => {
+  let sortedExp = Object.entries(global.DATABASE.data.users).sort((a, b) => b[1].exp - a[1].exp)
+  let sortedLim = Object.entries(global.DATABASE.data.users).sort((a, b) => b[1].limit - a[1].limit)
+  let usersExp = sortedExp.map(v => v[0])
+  let usersLim = sortedLim.map(v => v[0])
+  let len = args[0] && args[0].length > 0 ? Math.min(1000, Math.max(parseInt(args[0]), 5)) : Math.min(20, sortedExp.length)
   let text = `
-• *Top pendejos ${len}* •
-Kamu: *${usersExp.indexOf(m.sender) + 1}* dari *${usersExp.length}*
+• *Top 20 Mejores Pendejos  ${len}* •
+Tú: *${usersExp.indexOf(m.sender) + 1}* de *${usersExp.length}*
 
-${sortedExp.slice(0, len).map(({ jid, exp }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${exp} Exp*`).join`\n`}
+${sortedExp.slice(0, len).map(([user, data], i) => (i + 1) + '. @' + user.split`@`[0] + ': *' + data.exp + ' Exp*').join`\n`}
 
-• *Top medianamente de pendejos ${len}* •
-Kamu: *${usersLim.indexOf(m.sender) + 1}* dari *${usersLim.length}*
+• *Tabla de los pendejos mas pendejos ${len}* •
+Tú: *${usersLim.indexOf(m.sender) + 1}* de *${usersLim.length}*
 
-${sortedExp.slice(0, len).map(({ jid, limit }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${limit} Exp*`).join`\n`}
-
-• *Top 3 mayores pendejos de todos los tiempos ${len}* •
-Kamu: *${usersLevel.indexOf(m.sender) + 1}* dari *${usersLevel.length}*
-
-${sortedExp.slice(0, len).map(({ jid, level }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *Level ${level}*`).join`\n`}
+${sortedLim.slice(0, len).map(([user, data], i) => (i + 1) + '. @' + user.split`@`[0] + ': *' + data.limit + ' Limit*').join`\n`}
 `.trim()
   conn.reply(m.chat, text, m, {
     contextInfo: {
-      mentionedJid: [...usersExp.slice(0, len), ...usersLim.slice(0, len), ...usersLevel.slice(0, len)].filter(v => !participants.some(p => v === p.jid))
+      mentionedJid: [...usersExp.slice(0, len), ...usersLim.slice(0, len)]
     }
   })
 }
@@ -49,18 +38,3 @@ handler.exp = 0
 
 module.exports = handler
 
-function sort(property, ascending = false) {
-  if (property) return (...args) => args[!ascending & 1][property] - args[ascending & 1][property]
-  else return (...args) => args[!ascending & 1] - args[ascending & 1]
-}
-
-function toNumber(property, _default = 0) {
-  if (property) return (a, i, b) => {
-    return {...b[i], [property]: a[property] === undefined ? _default : a[property]}
-  }
-  else return a => a === undefined ? _default : a
-}
-
-function enumGetKey(a) {
-  return a.jid
-}
