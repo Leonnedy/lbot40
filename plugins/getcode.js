d673b2c4baaa1d025a86a9b7dd7cb60b8fa10597
Leonnedy <81530232+Leonnedy@users.noreply.github.com>
2021-04-08 20:30:02 -0300

    Update getcode.js

--- plugins/getcode.js
+++ plugins/getcode.js
@@ -2,9 +2,9 @@ let handler = async (m, { conn, usedPrefix }) => {
   if (global.conn.user.jid == conn.user.jid) conn.reply(m.chat, 'Este comando es solo para bots', m)
   else global.conn.reply(conn.user.jid, `${usedPrefix}jadibot ${Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString('base64')}`, m)
 }
-handler.help = ['getcode']
+handler.help = ['ObtenerCodigo']
 handler.tags = ['jadibot']
-handler.command = /^(getcode)$/i
+handler.command = /^(obcodigo)$/i
 handler.owner = true
 handler.mods = false
 handler.premium = false
