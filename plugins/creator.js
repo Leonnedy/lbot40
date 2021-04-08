d1be001f7dce1f3d1a603ce1fc0ff2b01636b81b
Leonnedy <81530232+Leonnedy@users.noreply.github.com>
2021-04-08 19:36:51 -0300

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,10 +1,10 @@
 let handler = function (m) {
   // this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
-  this.sendContact(m.chat, '0', '18292097057', m)
+  this.sendContact(m.chat, '0', '+1 829 209 7057', m)
 }
-handler.help = ['owner', 'creator']
+handler.help = ['owner', 'creador']
 handler.tags = ['info']
 
-handler.command = /^(owner|creator)$/i
+handler.command = /^(owner|creador)$/i
 
 module.exports = handler
