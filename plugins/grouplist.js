38c4ff16efd6784c6fa1a76a82d80431cc68b415
Leonnedy <81530232+Leonnedy@users.noreply.github.com>
2021-04-08 20:35:36 -0300

    Update grouplist.js

--- plugins/grouplist.js
+++ plugins/grouplist.js
@@ -2,9 +2,9 @@ let handler = async (m, { conn }) => {
   let txt = conn.chats.array.filter(v => v.jid.endsWith('g.us')).map(v =>`${conn.getName(v.jid)}\n${v.jid} [${v.read_only ? 'Left' : 'Joined'}]`).join`\n\n`
   conn.reply(m.chat, 'Lista de Grupos:\n' + txt, m)
 }
-handler.help = ['groups', 'grouplist']
+handler.help = ['grupos', 'listaDeGrupos']
 handler.tags = ['info']
-handler.command = /^(group(s|list))$/i
+handler.command = /^(grupo(s|lista))$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
