c5e1aad6db53cb7d616bf5d4542e15e00e544d3a
Leonnedy <81530232+Leonnedy@users.noreply.github.com>
2021-04-08 19:01:29 -0300

    Update buylimit.js

--- plugins/buylimit.js
+++ plugins/buylimit.js
@@ -1,6 +1,6 @@
 const xpperlimit = 350 
 let handler = async (m, { conn, command, args }) => {
-  let count = command.replace(/^buy/i, '')
+  let count = command.replace(/^comprar/i, '')
   count = count ? /all/i.test(count) ? Math.floor(global.DATABASE._data.users[m.sender].exp / xpperlimit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
   count = Math.max(1, count)
   if (global.DATABASE._data.users[m.sender].exp >= xpperlimit * count) {
@@ -9,7 +9,7 @@ let handler = async (m, { conn, command, args }) => {
     conn.reply(m.chat, `-${xpperlimit * count} XP\n+ ${count} Limit`, m)
   } else conn.reply(m.chat, `Ya mamaste, se te acabaron los puntos ${count} limit`, m)
 }
-handler.help = ['buy<jumlah limit>', 'buy <jumlah limit>', 'buyall']
+handler.help = ['Comprar<número de límites>', 'comprar <número de límites>', 'comprartodo']
 handler.tags = ['xp']
 handler.command = /^comprar([0-9]+)|buy|comprartodo$/i
 handler.owner = false
