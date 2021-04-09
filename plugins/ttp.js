7160d3498c8c99880561eb1ed483e09fb50231b5
Leonnedy <81530232+Leonnedy@users.noreply.github.com>
2021-04-08 21:32:11 -0300

    Update ttp.js

--- plugins/ttp.js
+++ plugins/ttp.js
@@ -2,13 +2,13 @@ const { sticker } = require('../lib/sticker')
 const { MessageType } = require('@adiwajshing/baileys')
 
 let handler = async (m, { conn, text }) => {
-  if (!text) throw 'PONE UN TEXTO HIJO DE PUTA! NONO VOY A IRME A LLORAR COMO BOT AAAAAA'
+  if (!text) throw 'PONE UN TEXTO HIJO DE PUTA YA ME TIENEN HASTA EL ORTO! NONO AHORA VOY A IRME A LLORAR COMO BOT NO ME JODAN AAAAAA'
   let stiker = await sticker(null, global.API('xteam', '/ttp', { file: '', text }), global.packname, global.author)
   conn.sendMessage(m.chat, stiker, MessageType.sticker, {
     quoted: m
   })
 }
-handler.help = ['ttp <teks>']
+handler.help = ['ttp <texto>']
 handler.tags = ['sticker']
 handler.command = /^ttp$/i
 handler.owner = false
