95a87578f0d8b1ca59f8fa700fd3465eb9e18d84
Leonnedy <81530232+Leonnedy@users.noreply.github.com>
2021-04-09 14:13:09 -0300

    Update Grup-seting.js

--- plugins/Grup-seting.js
+++ plugins/Grup-seting.js
@@ -1,22 +1,22 @@
 let { Presence, GroupSettingChange } = require('@adiwajshing/baileys')
 let handler  = async (m, { conn, args, usedPrefix, command }) => {
 	let isClose = { // Switch Case Like :v
-		'open': false,
-		'close': true,
+		'abierto': false,
+		'cerrado': true,
 	}[(args[0] || '')]
 	await conn.updatePresence(m.chat, Presence.composing)
 	if (isClose === undefined)
 		throw `
-*Format salah! Contoh :*
+*¡Formato incorrecto! Ejemplo:*
 
-  *○ ${usedPrefix + command} close*
-  *○ ${usedPrefix + command} open*
+  *○ ${usedPrefix + command} cerrado*
+  *○ ${usedPrefix + command} abierto*
 `.trim()
 	await conn.groupSettingChange(m.chat, GroupSettingChange.messageSend, isClose)
 }
-handler.help = ['group *open / close*']
+handler.help = ['grupo *abierto / cerrado*']
 handler.tags = ['group']
-handler.command = /^(group)$/i
+handler.command = /^(grupo)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
