6c6eade9f7b6802c2baedefbff21ef4f2ae96542
Leonnedy <81530232+Leonnedy@users.noreply.github.com>
2021-04-08 21:04:27 -0300

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -130,9 +130,9 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
     throw e
   }
 }
-handler.help = ['menu','help','?']
+handler.help = ['menu','ayuda','?']
 handler.tags = ['main']
-handler.command = /^(menu|help|\?)$/i
+handler.command = /^(menu|ayuda|\?)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
