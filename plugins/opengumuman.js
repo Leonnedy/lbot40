c732e5bdf1b985d04cc52a41b774dd63595bc741
Leonnedy <81530232+Leonnedy@users.noreply.github.com>
2021-04-08 21:08:52 -0300

    Update opengumuman.js

--- plugins/opengumuman.js
+++ plugins/opengumuman.js
@@ -4,9 +4,9 @@ let handler = async (m, { conn, text }) => {
   let users = (await conn.groupMetadata(m.chat)).participants.map(u => u.jid)
   conn.sendMessage(m.chat, text, MessageType.extendedText, { contextInfo: { mentionedJid: users } })
 }
-handler.help = ['pengumuman','hidetag'].map(v => 'o' + v + ' [teks]')
+handler.help = ['anuncio','hidetag'].map(v => 'o' + v + ' [teks]')
 handler.tags = ['owner']
-handler.command = /^(opengumuman|oannounce|ohiddentag|ohidetag)$/i
+handler.command = /^(anuncio|oannounce|ohiddentag|ohidetag)$/i
 handler.owner = true
 handler.mods = false
 handler.premium = false
