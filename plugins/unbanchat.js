18911a448a5b2e48d4add7b6bf5dd7dc45f90657
Leonnedy <81530232+Leonnedy@users.noreply.github.com>
2021-04-08 21:33:33 -0300

    Update unbanchat.js

--- plugins/unbanchat.js
+++ plugins/unbanchat.js
@@ -2,9 +2,9 @@ let handler = async (m, { conn }) => {
   global.DATABASE._data.chats[m.chat].isBanned = false
   m.reply('Done!')
 }
-handler.help = ['unbanchat']
+handler.help = ['desbancar chat']
 handler.tags = ['owner']
-handler.command = /^unbanchat$/i
+handler.command = /^desbanchat$/i
 handler.owner = true
 
 module.exports = handler
