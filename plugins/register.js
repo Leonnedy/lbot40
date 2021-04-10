445ea84eacefb7b386456318b6ea2037492d18d8
Leonnedy <81530232+Leonnedy@users.noreply.github.com>
2021-04-09 22:56:49 -0300

    Update register.js

--- plugins/register.js
+++ plugins/register.js
@@ -2,11 +2,11 @@ const { createHash } = require('crypto')
 let Reg = /(.*)([.|])([0-9]*)$/i
 let handler = async function (m, { text, usedPrefix }) {
   let user = global.DATABASE._data.users[m.sender]
-  if (user.registered === true) throw `Anda sudah terdaftar\nQuiere volver a registrarse? ${usedPrefix}unreg <SN|SERIAL NUMBER>`
-  if (!Reg.test(text)) throw `Foemat salah\n*${usedPrefix}daftar <nama>.umur>*`
+  if (user.registered === true) throw `Usted ya está registrado\nQuiere volver a registrarse? ${usedPrefix}unreg <SN|SERIAL NUMBER>`
+  if (!Reg.test(text)) throw `Formato incorrecto\n*${usedPrefix}daftar <nama>.umur>*`
   let [_, name, splitter, age] = text.match(Reg)
-  if (!name) throw 'Nama tidak boleh kosong (Alphanumeric)'
-  if (!age) throw 'Umur tidak boleh kosong (Angka)'
+  if (!name) throw 'El nombre no puede estar vacío (Alphanumeric)'
+  if (!age) throw 'La edad no puede estar vacía (Cifra)'
   user.name = name
   user.age = parseInt(age)
   user.regTime = + new Date
