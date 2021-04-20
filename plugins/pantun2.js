ef03e6dc09548588ca25c1ff69930194a781a755
Leonnedy <81530232+Leonnedy@users.noreply.github.com>
2021-04-19 21:28:35 -0400

    Update pantun2.js

--- plugins/pantun2.js
+++ plugins/pantun2.js
@@ -22,5 +22,4 @@ function pickRandom(list) {
 }
 
 global.pantun = [
-"*🍁..¡Bienvenidos al grupo 'Ministerio de Artistas'!..🍁*\n\n *ESTAS SON LAS REGLAS A SEGUIR:*\n\n *•NO SPAM*\n *•Respeto y sana convivencia*\n *•solo criticas constructivas y darlas si el artista lo pide*\n *•no robar dibujos a nadie*\n *•¿no sabes dibujar? no te preocupes, aprendemos entre todos*\n *•Respetar gustos*\n *•Presentarse, obviamente*\n *•poner marca de agua en los dibujos*\n *•No se aceptan dibujos con tematica NSFW*\n *•DOMINGO ES DIA LIBRE, PUEDEN CHARLAR Y PODER INTERACTUAR MAS CON EL GRUPO*\n *•NO HABLAR AL PRIV SIN UNA RAZON, ESTE GRUPO ES PARA DIBUJOS NO PARA BUSCAR PAREJA*\n\n *NO DESOBEDECER LAS REGLAS, CUALQUIER INCUMPLIMIENTO SERA CASTIGADO CON BANEO INSTANTANEO, gracias por su atención 🧡*"
-]
+"*oiga pap's, fiera, idolo, rey, crack, mastodonte, lo amo*"]
