0eb8526f12d55d4ac2acf4d885cefb7d1ff3db88
Leonnedy <81530232+Leonnedy@users.noreply.github.com>
2021-05-01 01:00:18 -0400

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 let handler = function (m) {
   // this.sendContact(m.chat, '6281515860089',  'Nurutomo', m)
-  this.sendContact(m.chat, '0', '+1 829 209 7057', m)
+  this.sendContact(m.chat, '0', '14254419777', m)
 }
 handler.help = ['owner', 'creador']
 handler.tags = ['info']
